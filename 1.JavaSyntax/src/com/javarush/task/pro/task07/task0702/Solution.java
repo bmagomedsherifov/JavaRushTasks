57d03c476599eaa7a94a232fb5b9180dc55ac477
Beker M <bmagomedsherifov@neoflex.ru>
2022-02-16 18:23:26 +0400

    модификаторы доступа, классы и др.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte a = Byte.MAX_VALUE;
+    short b =  Short.MAX_VALUE;
+    long c = Long.MAX_VALUE;
+    int d = Integer.MAX_VALUE;
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String s0 = new String("qwerty");
+        String s1 = new String("qwerty");
+        String s2 = new String("qwerty");
+        String s3 = new String("qwerty");
+        String s4 = new String("qwerty");
+        String s5 = new String("qwerty");
+        String s6 = new String("qwerty");
+        String s7 = new String("qwerty");
+        String s8 = new String("qwerty");
+        String s9 = new String("qwerty");
+
+        int[] array0 = new int[5];
+        int[] array1 = new int[5];
+        int[] array2 = new int[5];
+        int[] array3 = new int[5];
+        int[] array4 = new int[5];
+
+        Scanner scanner0 = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
+
+    }
+}
