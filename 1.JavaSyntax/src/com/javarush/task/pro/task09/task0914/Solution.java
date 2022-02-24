ea8e50da06204441584efb1a86eb97b279d4ab06
Beker M <bmagomedsherifov@neoflex.ru>
2022-02-24 11:21:54 +0400

    + stringbuiled и прочее

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        int index1 = path.indexOf("jdk");
+        int index2 = path.indexOf("/", index1);
+        String jdk1 = path.substring(index1, index2);
+
+        return path.replace(jdk1, jdk);
+    }
+}
